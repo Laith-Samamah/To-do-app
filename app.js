b03f561457e407d13938de559e4e0cbcda20f34d
Laith-Samamah <Laith.samamah@gmail.com>
2022-08-11 19:18:45 +0300

    loops

--- app.js
+++ app.js
@@ -1,28 +1,98 @@
-let Name = prompt("Enter your name");
+// let Name = prompt("Enter your name");
 
-let gender = prompt("Enter your gender (the answer should be male or female only)");
-switch (gender) {
-        case "male" :
-        case "Male" :
-        Name= "Mr." + " " + Name
-        break;
+// let gender = prompt("Enter your gender (the answer should be male or female only)");
+// switch (gender) {
+//         case "male" :
+//         case "Male" :
+//         Name= "Mr." + " " + Name
+//         break;
         
-        case "female":
-        case "Female":
-        Name= "Ms." + " " + Name
-        break;
+//         case "female":
+//         case "Female":
+//         Name= "Ms." + " " + Name
+//         break;
+// }
 
+// let age = prompt("How old are you?");
+
+// if (age <=0 ) {
+//     alert("your age is invalid, age must be greater than zero");
+//     age = prompt("How old are you?");
+// }
+
+// let confirmation = confirm("Do you want to view a welcome message?");
+// if (confirmation == true) {
+//     alert("Hello "+ Name )
+// }
+
+let Q1 = prompt("Is this your first time using this website?\n(Yes or No)");
+switch (Q1) {
+    case "yes":
+    case "y" :
+     Q1="Yes"
+     break;
+
+    case "no" :
+    case "n" :
+     Q1="No"
+     break;
+
+    default:
+        Q1= "Invalid"
+    
 }
 
-let age = prompt("How old are you?");
+let Q2 = prompt("Do you like movies?\n(Yes or No)");
+switch (Q2) {
+    case "yes":
+    case "y" :
+     Q2="Yes"
+     break;
+     
+    case "no" :
+    case "n" :
+     Q2="No"
+     break;
 
-if (age <=0 ) {
-    alert("your age is invalid, age must be greater than zero");
-    age = prompt("How old are you?");
+    default:
+        Q2= "Invalid"
 }
 
-let confirmation = confirm("Do you want to view a welcome message");
-if (confirmation == true) {
-    alert("Hello "+ Name )
+
+let Q3 = prompt("Do you watch movies every day?\n(Yes or No)");
+switch (Q3) {
+    case "yes":
+    case "y" :
+      Q3="Yes"
+         break;
+         
+        case "no" :
+        case "n" :
+         Q3="No"
+         break;
+    
+        default:
+            Q3= "Invalid"
 }
 
+const answers=[Q1,Q2,Q3];
+console.log(answers);
+
+let rate = prompt("From 0 to 5, how many stars do you rate this website?\n(0 is the worst - 5 is the best)");
+
+while (rate < 0) {
+    rate = 0
+    alert("Thank you for your review" + "\nYour rate is " + rate)
+    break;
+}
+
+while (rate > 5) {
+    rate = 5
+    alert("Thank you for your review" + "\nYour rate is " + rate)
+    break;
+}
+
+while ( rate >= 0 && rate <= 5  ) {
+    alert("Thank you for your review" + "\nYour rate is " + rate)
+    break;
+}
